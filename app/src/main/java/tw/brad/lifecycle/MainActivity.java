2a8543c74df1896aaec181e7a7397460549ed06b
Brad Chao <brad@brad.tw>
2016-09-12 10:22:05 +0800

    v2

--- app/src/main/java/tw/brad/lifecycle/MainActivity.java
+++ app/src/main/java/tw/brad/lifecycle/MainActivity.java
@@ -1,17 +1,74 @@
 package tw.brad.lifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MainActivity(){
+        Log.d("brad", "MainActivity");
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("brad", "onCreate");
     }
-    public void gotoPage2(View v){
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("brad", "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("brad", "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("brad", "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("brad", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("brad", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("brad", "onDestroy");
+    }
+
+    public void gotoPage2(View v){
+        Intent intent = new Intent(this, Page2Activity.class);
+        intent.putExtra("name","Brad");
+        intent.putExtra("stage",4);
+        intent.putExtra("sound",false);
+        startActivity(intent);
+    }
+    public void exit(View v){
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        Log.d("brad", "finish");
     }
 }
